e1a9b579223736a9e9b9fada66c34c4ec773747f
M. MLynn <mlynn1710@gmail.com>
2017-04-17 11:31:02 +0800

    edit the SignUP activity

---
+++ app/src/main/java/marlyn/registerloginlogoutsqlite/Welcome.java
@@ -0,0 +1,27 @@
+package marlyn.registerloginlogoutsqlite;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by marLyn on 4/16/2017.
+ */
+
+public class Welcome extends Activity {
+    ImageView dpImage;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+
+
+
+    }
+}
