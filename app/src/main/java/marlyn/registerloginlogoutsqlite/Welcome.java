2e6f13db68c42106aa33fb793e9ff21b68aad658
M. MLynn <mlynn1710@gmail.com>
2017-04-17 13:05:30 +0800

    add change image

--- app/src/main/java/marlyn/registerloginlogoutsqlite/Welcome.java
+++ app/src/main/java/marlyn/registerloginlogoutsqlite/Welcome.java
@@ -2,6 +2,9 @@ package marlyn.registerloginlogoutsqlite;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,11 +13,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+
 
 public class Welcome extends AppCompatActivity {
 
     ImageView disimage;
-
+    private static final int SELECT_PHOTO = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,15 @@ public class Welcome extends AppCompatActivity {
 
         String loginName = intent.getStringExtra("USERNAME");
         txtname.setText(loginName);
+
+        disimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            }
+        });
 
     }
 
@@ -56,6 +70,59 @@ public class Welcome extends AppCompatActivity {
 
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    //this Method call when user pick an image from ImagePicker. e.g gallery
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+
+                    Bitmap yourSelectedImage = null;
+                    try {
+                        //decodeUri() Method Defined Below
+                        yourSelectedImage = decodeUri(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    disimage.setImageBitmap(yourSelectedImage);
+                }
+        }
+    }
+
+    //decodeUri() Method for decoding image for Out of Memory Exception
+    private Bitmap decodeUri(Uri selectedImage) throws FileNotFoundException {
+
+        // Decode image size
+        BitmapFactory.Options o = new BitmapFactory.Options();
+        o.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage), null, o);
+
+        // The new size we want to scale to
+        final int REQUIRED_SIZE_W = 140;
+        final int REQUIRED_SIZE_H = 140;
+        // Find the correct scale value. It should be the power of 2.
+        int width_tmp = o.outWidth, height_tmp = o.outHeight;
+        int scale = 1;
+        while (true) {
+            if (width_tmp / 2 < REQUIRED_SIZE_W
+                    || height_tmp / 2 < REQUIRED_SIZE_H) {
+                break;
+            }
+            width_tmp /= 2;
+            height_tmp /= 2;
+            scale *= 2;
+        }
+
+        // Decode with inSampleSize
+        BitmapFactory.Options o2 = new BitmapFactory.Options();
+        o2.inSampleSize = scale;
+        return BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage), null, o2);
+
     }
 
 }
